3dd2bb681555501edcbfe6626d7032dd011f34ac
Shane Cowherd <shane.cowherd@me.com>
2012-08-19 02:01:33 -0600

    Added Map

--- static/js/home.js
+++ static/js/home.js
@@ -1,27 +1,55 @@
 jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - this.outerHeight()) / 2) + 
-                $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - this.outerWidth()) / 2) + 
-                $(window).scrollLeft()) + "px");
-    return this;
+  this.css("position","absolute");
+  this.css("top", Math.max(0, (($(window).height() - this.outerHeight()) / 2) + 
+        $(window).scrollTop()) + "px");
+  this.css("left", Math.max(0, (($(window).width() - this.outerWidth()) / 2) + 
+        $(window).scrollLeft()) + "px");
+  return this;
 }
 
 $(document).ready(function() {
-    $('.show_request').click(function(){
-        $('#request_form').center();
-        $('#modal_background').fadeToggle();
-        $('#request_form').fadeToggle();   
-    });
-    $('.make_offer').click(function(){
-        $('#offer_form').center();
-        $('#modal_background').fadeToggle();
-        $('#offer_form').fadeToggle();   
-    });
-    $('#modal_background').click(function(){
-        $(this).fadeToggle();
-        $('#request_form:visible').toggle();
-        $('#offer_form:visible').toggle();
+  $('.show_request').click(function(){
+    $('#request_form').center();
+    $('#modal_background').fadeToggle();
+    $('#request_form').fadeToggle();   
+    initializeMap(36.14342011686289,-96.00540148632808);
+  });
+  $('.make_offer').click(function(){
+    $('#offer_form').center();
+    $('#modal_background').fadeToggle();
+    $('#offer_form').fadeToggle();   
+  });
+  $('#modal_background').click(function(){
+    $(this).fadeToggle();
+    $('#request_form:visible').toggle();
+    $('#offer_form:visible').toggle();
+  });
 
-    });
+    
 });
+
+
+
+function initializeMap(lat,lng) {
+  var myLatlng = new google.maps.LatLng(lat,lng);
+  var myOptions = {
+    zoom: 4,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLatlng, 
+      map: map,
+      draggable:true
+  });
+  google.maps.event.addListener(
+      marker,
+      'drag',
+      function() {
+        $('#id_latitude').val(marker.position.lat().toFixed(7));
+        $('#id_longitude').val(marker.position.lng().toFixed(7));
+      }
+      );
+}
