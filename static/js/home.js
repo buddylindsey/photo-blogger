d6e1b75b6b0738b8d5788b9be24498cba54e436c
Shane Cowherd <shane.cowherd@me.com>
2012-08-19 17:34:57 -0600

    Added Thumbnail Views

--- static/js/home.js
+++ static/js/home.js
@@ -103,7 +103,7 @@ $(document).ready(function() {
     $(this).remove();
   }
   });
-
+  imagePreview();
 });
 
 
@@ -131,3 +131,35 @@ function initializeMap(mapidname,lat,lng) {
       }
       );
 }
+
+this.imagePreview = function(){	
+	/* CONFIG */
+		
+		xOffset = 10;
+		yOffset = 30;
+		
+		// these 2 variable determine popup's distance from the cursor
+		// you might want to adjust to get the right result
+		
+	/* END CONFIG */
+	$("a.preview").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<p id='preview'><img src='"+ this.href +"' alt='Image preview' />"+ c +"</p>");								 
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+	function(){
+		this.title = this.t;	
+		$("#preview").remove();
+    });	
+	$("a.preview").mousemove(function(e){
+		$("#preview")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
