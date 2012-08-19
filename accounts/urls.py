40a7276bc002f48e3290a90aca5a6a52e27c6bd1
Shane Cowherd <shane.cowherd@me.com>
2012-08-18 19:05:49 -0600

    Created logout button

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns('',
     url(r'', include('social_auth.urls')),
     url(r'^index/$', TemplateView.as_view(template_name="accounts/index.html"), name="account_index"),
     url(r'^login/$', redirect_to, { 'url': '/accounts/login/facebook' }),
-    url(r'^logout/$', auth_logout, name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+                          {'next_page': '/'}),
+    url(r'^$', redirect_to, { 'url': '/' }),
 )
